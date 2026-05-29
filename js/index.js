e8e1c4ae86c22da994c6a6c0bb92274bd460980e
mildredlilithmezacastro-hash <mildredlilithmezacastro@gmail.com>
2026-05-28 21:18:19 -0600

    agregando base de datos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo');
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -5,4 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // add recipe form
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
+});
+
+btnAgregarPlatillo.addEventListener('click', function(){
+  alert('Platillo agregado');
 });
