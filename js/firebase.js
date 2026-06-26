b35871c48e7aae1acaedeba8deeb90cea69ac94e
mildredlilithmezacastro-hash <mildredlilithmezacastro@gmail.com>
2026-06-25 21:21:37 -0600

    se agrego la lista desplegable de platillos en la seccion pedidos

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDviwbyRQZS4qdQQy1c3Wrh1hoPOAsYB60",
+    authDomain: "ubereatsmildred-df369.firebaseapp.com",
+    projectId: "ubereatsmildred-df369",
+    storageBucket: "ubereatsmildred-df369.firebasestorage.app",
+    messagingSenderId: "62470196385",
+    appId: "1:62470196385:web:282c8b8bea5295c59c34e0"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
